0a1453e847ee941a67e524a2a7962d2e57b2d75d
hiromu624 <hiromu.music.624@gmail.com>
2021-10-29 09:46:58 +0000

    validation commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,11 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    if book.save
+      redirect_to book_path(book.id), notice: "Book was successfully created."
+    else
+      redirect_to books_path
+    end
   end
 
   def edit
@@ -20,8 +23,12 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      redirect_to book_path(book.id), notice: "Book was successfully updated."
+    else
+
+
+    end
   end
 
   def destroy
